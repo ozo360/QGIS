679c02620d849d46559eecd62c3b9b0ea29c43c3
ozaki <ozaki@localhost>
2018-04-11 22:50:32 +0900

    Apply voronoi.patch https://issues.qgis.org/issues/8002 Eliminate bug that polygons overlap

--- python/plugins/processing/algs/qgis/VoronoiPolygons.py
+++ python/plugins/processing/algs/qgis/VoronoiPolygons.py
@@ -16,6 +16,10 @@
 *                                                                         *
 ***************************************************************************
 """
+# For comparing equality in lat/long - 10^6 meters are < 1 degree
+# so this gives mm resolution at lat=0. For finer resolution tasks
+# a localised projection will likely be used, so this should be OK.
+FLT_EPSILON = 10**-9
 
 __author__ = 'Victor Olaya'
 __date__ = 'August 2012'
@@ -23,7 +27,7 @@ __copyright__ = '(C) 2012, Victor Olaya'
 
 # This will get replaced with a git SHA1 when you do a git archive
 
-__revision__ = '$Format:%H$'
+__revision__ = 'f378a23ed8e56d225ab5734766fc26fa5d832369'
 
 import os
 
@@ -157,44 +161,94 @@ class VoronoiPolygons(QgisAlgorithm):
         """Clip voronoi function based on code written for Inkscape.
         Copyright (C) 2010 Alvin Penner, penner@vaxxine.com
         """
+        def float_eq( a, b ):
+            # the initial == test is for efficiency
+            return a == b or abs( a - b ) <= FLT_EPSILON
 
-        def clip_line(x1, y1, x2, y2, w, h):
+        def clip_line(x1, y1, x2, y2, w, h,):
+            # where both x's are left of the clipping region no line needed
             if x1 < 0 and x2 < 0:
                 return [0, 0, 0, 0]
+            # where both x's are right of the clipping region no line needed
             if x1 > w and x2 > w:
                 return [0, 0, 0, 0]
-            if x1 < 0:
-                y1 = (y1 * x2 - y2 * x1) / (x2 - x1)
-                x1 = 0
-            if x2 < 0:
-                y2 = (y1 * x2 - y2 * x1) / (x2 - x1)
-                x2 = 0
-            if x1 > w:
-                y1 = y1 + (w - x1) * (y2 - y1) / (x2 - x1)
-                x1 = w
-            if x2 > w:
-                y2 = y1 + (w - x1) * (y2 - y1) / (x2 - x1)
-                x2 = w
+            # where both y's are below the clipping region no line needed
             if y1 < 0 and y2 < 0:
                 return [0, 0, 0, 0]
+            # where both y's are above the clipping region no line needed
             if y1 > h and y2 > h:
                 return [0, 0, 0, 0]
-            if x1 == x2 and y1 == y2:
+            # where end points are too close to tell apart no line needed
+            if float_eq( x1, x2 ) and float_eq( y1, y2 ):
                 return [0, 0, 0, 0]
-            if y1 < 0:
-                x1 = (x1 * y2 - x2 * y1) / (y2 - y1)
+            # y = mx + b: calculate gradient but make sure is not a vertical line
+            if not float_eq( x1, x2 ):
+                m = (y2 - y1) / (x2 - x1)
+            else:
+                m = float("+inf") # vertical lines have infinite gradient
+            # y = mx + b: calculate intercept, if horizontal m==0 so use first Y value
+            if ( not float_eq( y1, y2 ) and not float_eq( x1, x2 ) ) or float_eq( y1, y2):
+                b = y1 - m*x1 # b = y1 when y1 == y2
+            else: # y1 != y2 and x1 == x2
+                b = float("nan") # vertical lines have no intercept
+            # for simplicity later define if the line is vertical
+            is_vertical = ( m == float("+inf") and b == float("nan") )
+            # where either of the x1,y1 or x2,y2 points are outside of the clipping region
+            # need to move them to be on the border of the region; this is done by recalculating
+            # the x or y value based on the equation of the line holding the other axis as the
+            # value of the clipping rectangle border that is violated.
+            if x1 < 0: # first point is left of region
+                # impossible to have vertical line here, could be horizontal
+                # but that doesn't matter as m = 0, b = y1 in that case
+                x1 = 0
+                y1 = m * x1 + b
+            if x2 < 0: # second point is left of region
+                # impossible to have vertical line here, could be horizontal
+                # but that doesn't matter as m = 0, b = y1 in that case
+                x2 = 0
+                y2 = m * x2 + b
+            if x1 > w: # first point is right of region
+                # impossible to have vertical line here, could be horizontal
+                # but that doesn't matter as m = 0, b = y1 in that case
+                x1 = w
+                y1 = m * x1 + b
+            if x2 > w: # second point is right of region
+                # impossible to have vertical line here, could be horizontal
+                # but that doesn't matter as m = 0, b = y1 in that case
+                x2 = w
+                y2 = m * x2 + b
+            if y1 < 0: # first point is below the region
+                # impossible to have horizontal line here, could be vertical though
+                # if is vertical then x value does not need to change
                 y1 = 0
-            if y2 < 0:
-                x2 = (x1 * y2 - x2 * y1) / (y2 - y1)
+                if not is_vertical: # if veritical then X doesn't need to change
+                    x1 = (y1 - b) / m
+            if y2 < 0: # second point is below the region
+                # impossible to have horizontal line here, could be vertical though
+                # if is vertical then x value does not need to change
                 y2 = 0
-            if y1 > h:
-                x1 = x1 + (h - y1) * (x2 - x1) / (y2 - y1)
+                if not is_vertical: # if vertical then X doesn't need to change
+                    x2 = (y2 - b) / m
+            if y1 > h: # first point is above the region
+                # impossible to have horizontal line here, could be vertical though
+                # if is vertical then x value does not need to change
                 y1 = h
-            if y2 > h:
-                x2 = x1 + (h - y1) * (x2 - x1) / (y2 - y1)
-                y2 = h
-            return [x1, y1, x2, y2]
 
+                if not is_vertical: # if vertical then X doesn't need to change
+                    x1 = (y1 - b) / m
+            if y2 > h: # second point is above the region
+                # impossible to have horizontal line here, could be vertical though
+                # if is vertical then x value does not need to change
+                y2 = h
+
+                if not is_vertical: # if vertical then X doesn't need to change
+                    x2 = (y2 - b) / m
+            # recheck point approximate equivalance because both points may have been
+            # outside the region and thus were both clipped to a point on the boundary
+            if float_eq( x1, x2 ) and float_eq( y1, y2 ):
+                return [ 0, 0, 0, 0 ]
+            # return the clipped region
+            return [x1, y1, x2, y2]
         lines = []
         hasXMin = False
         hasYMin = False
@@ -209,20 +263,20 @@ class VoronoiPolygons(QgisAlgorithm):
                     c.vertices[edge[2]][0],
                     c.vertices[edge[2]][1],
                     width,
-                    height
+                    height,
                 )
             elif edge[1] >= 0:
                 # Only one vertex
                 if c.lines[edge[0]][1] == 0:
                     # Vertical line
-                    xtemp = c.lines[edge[0]][2] / c.lines[edge[0]][0]
-                    if c.vertices[edge[1]][1] > (height) / 2:
+                    xtemp = c.vertices[edge[1]][0]
+                    if c.vertices[edge[1]][1] > height / 2:
                         ytemp = height
                     else:
-                        ytemp = 0
+                        ytemp = 0.0
                 else:
                     xtemp = width
-                    ytemp = (c.lines[edge[0]][2] - width *
+                    ytemp = (c.lines[edge[0]][2] - xtemp *
                              c.lines[edge[0]][0]) / c.lines[edge[0]][1]
                 [x1, y1, x2, y2] = clip_line(
                     c.vertices[edge[1]][0],
@@ -230,36 +284,37 @@ class VoronoiPolygons(QgisAlgorithm):
                     xtemp,
                     ytemp,
                     width,
-                    height
+                    height,
                 )
             elif edge[2] >= 0:
                 # Only one vertex
                 if c.lines[edge[0]][1] == 0:
                     # Vertical line
-                    xtemp = c.lines[edge[0]][2] / c.lines[edge[0]][0]
-                    if c.vertices[edge[2]][1] > (height) / 2:
+                    xtemp = c.vertices[edge[2]][0]
+                    if c.vertices[edge[2]][1] > height / 2:
                         ytemp = height
                     else:
                         ytemp = 0.0
                 else:
                     xtemp = 0.0
-                    ytemp = c.lines[edge[0]][2] / c.lines[edge[0]][1]
+                    ytemp = (c.lines[edge[0]][2] - xtemp *
+                             c.lines[edge[0]][0]) / c.lines[edge[0]][1]
                 [x1, y1, x2, y2] = clip_line(
                     xtemp,
                     ytemp,
                     c.vertices[edge[2]][0],
                     c.vertices[edge[2]][1],
                     width,
-                    height
+                    height,
                 )
             if x1 or x2 or y1 or y2:
                 lines.append(QgsPointXY(x1 + extent.xMinimum(),
                                         y1 + extent.yMinimum()))
                 lines.append(QgsPointXY(x2 + extent.xMinimum(),
                                         y2 + extent.yMinimum()))
-                if 0 in (x1, x2):
+                if 0.0 in (x1, x2):
                     hasXMin = True
-                if 0 in (y1, y2):
+                if 0.0 in (y1, y2):
                     hasYMin = True
                 if height in (y1, y2):
                     hasYMax = True
